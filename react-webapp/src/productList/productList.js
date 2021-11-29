a877b4fe72198d1c2cd0eba6cca1e2e854713a92
raghavdhingra <raghav.dhingra15@gmail.com>
2021-11-29 22:46:26 +0530

    updated product list

--- react-webapp/src/productList/productList.js
+++ react-webapp/src/productList/productList.js
@@ -1,11 +1,78 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+
+import {
+  INDEX_NAME,
+  MEILI_API_KEY,
+  MEILI_SEARCH_BASE_API,
+} from "../utilities/constants";
 
 import "./productList.css";
 
-const ProductList = () => {
+const ProductList = ({ searchStr = "", order = "", filter = "", location }) => {
+  const [productList, setProductList] = useState([]);
+
+  useEffect(() => {
+    const apiData = {};
+
+    /*
+      Multiple sort can be achieved by using checkboxes,
+      but for now I've kept it to radio buttons for
+      easy understanding
+    */
+    if (filter && order) {
+      if (filter === "geo") {
+        apiData["sort"] = [
+          `_geoPoint(${location.latitude}, ${location.longitude}):${order}`,
+        ];
+      } else {
+        apiData["sort"] = [`${filter}:${order}`];
+      }
+    }
+
+    /*
+      We can implement debounce for lazy search,
+      if we don't want to send repetitive requests
+      on search query change
+    */
+    axios({
+      url: `${MEILI_SEARCH_BASE_API}/indexes/${INDEX_NAME}/search`,
+      method: "POST",
+      headers: {
+        "X-Meili-API-Key": MEILI_API_KEY,
+      },
+      data: {
+        q: searchStr,
+        ...apiData,
+      },
+    }).then(({ data: { hits } }) => setProductList(hits));
+  }, [searchStr, order, filter, location]);
+
   return (
     <div className='product-list-cmpt'>
-      <h2>ProductList</h2>
+      {productList && productList.length > 0 ? (
+        <ul className='product-list-wrapper'>
+          {productList.map((product) => {
+            const { id, name, price, store_name, rating, address } = product;
+
+            return (
+              <li key={id} className='product-list-item'>
+                <h2>
+                  {name} <span className='product-rating'>{rating}/5</span>
+                </h2>
+                <p>Price: ₹{price}</p>
+                <p>{store_name}</p>
+                <p>Address: {address}</p>
+                <p className='buy-button-wrapper'>
+                  <button className='buy-button'>Buy Now</button>
+                </p>
+              </li>
+            );
+          })}
+        </ul>
+      ) : (
+        <h3 className='no-result'>No result found</h3>
+      )}
     </div>
   );
 };
---
+++ react-webapp/src/utilities/constants.js
@@ -0,0 +1,3 @@
+export const MEILI_SEARCH_BASE_API = "http://localhost:7700";
+export const MEILI_API_KEY = "a2efjndskfkdsj";
+export const INDEX_NAME = "product";
